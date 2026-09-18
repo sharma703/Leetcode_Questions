9bcc2148d60efa323776059eceb2159cfe09fc04
Nikhil Sharma <9090nikhilsharma@gmail.com>
2026-09-19 00:08:52 +0530

    Time: 0 ms (100%), Space: 9.7 MB (74.94%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,10 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        if(s == t) return true;
-        else return false;
-        
+        if(s.size() != t.size()) return false;
+
+        int freq[26] = {0};
+
+        for(int i = 0; i < s.size(); i++){
+            freq[s[i] - 'a']++;
+            freq[t[i] - 'a']--;
+        }
+
+        for(int i = 0; i < 26; i++){
+            if(freq[i] != 0) return false;
+        }
+        return true;
     }
 };
